8e1352a2d8a493538abf8e58777ff337292e65cc
Clive Galway <evilc@evilc.com>
2017-07-17 00:03:31 +0100

    Profile hotkeys test

--- src/HotkeyWrapper/HotkeyWrapper.cs
+++ src/HotkeyWrapper/HotkeyWrapper.cs
@@ -10,15 +10,34 @@ using System.Threading.Tasks;
 public class HotkeyWrapper
 {
     AutoHotkeyEngine bindModeThread = new AutoHotkeyEngine();
+    Dictionary<string, Profile> profileInstances = 
+        new Dictionary<string, Profile>(StringComparer.OrdinalIgnoreCase);
+    Dictionary<string, dynamic> hotkeyCallbacks =
+        new Dictionary<string, dynamic>(StringComparer.OrdinalIgnoreCase);
 
+    private const string defaultProfileName = "0";
     public HotkeyWrapper()
     {
-
         InitBindMode();
-        bindModeThread.Eval(String.Format("bh.SetDetectionState(1)"));
+        AddProfile(defaultProfileName);    // Default profile
+        //bindModeThread.Eval(String.Format("bh.SetDetectionState(1)"));
+    }
 
-        while (true)
-            Thread.Sleep(100);
+    public bool AddProfile(string profileName)
+    {
+        if (profileInstances.ContainsKey(profileName))
+            return false;
+        profileInstances.Add(profileName, new Profile(profileName, ProfileCallback));
+        return true;
+    }
+
+    public bool SetHotkey(string hkName, uint keyCode, dynamic callback, string profileName = defaultProfileName)
+    {
+        if (!profileInstances.ContainsKey(profileName))
+            return false;
+        hotkeyCallbacks[hkName] = callback;
+        profileInstances[profileName].SetHotkey(hkName, keyCode);
+        return true;
     }
 
     private void InitBindMode()
@@ -30,11 +49,62 @@ public class HotkeyWrapper
 
     static void BindModeEventCallback(bool evt, uint keyCode)
     {
-        Console.WriteLine(evt + ", " + keyCode);
+        Console.WriteLine("BindMode: " + evt + ", " + keyCode);
+    }
+
+    void ProfileCallback(string hkName, bool evt, uint keyCode)
+    {
+        hotkeyCallbacks[hkName](evt);
+        //Console.WriteLine("Profile: " + evt + ", " + keyCode);
     }
 
     [UnmanagedFunctionPointer(CallingConvention.StdCall)]
     delegate void BindModeEvent([MarshalAs(UnmanagedType.Bool)]bool evt, [MarshalAs(UnmanagedType.U4)]uint keyCode);
 
     static BindModeEvent bindModeEventDelegate = BindModeEventCallback;
+
+    private class Profile
+    {
+        string profileName;
+        AutoHotkeyEngine ahkThread = new AutoHotkeyEngine();
+        dynamic profileCallback;
+
+        public Profile(string name, Action<string, bool, uint> callback)
+        {
+            profileName = name;
+            profileCallback = callback;
+            ProfileEvent profileEventDelegate = profileEventCallback;
+
+            IntPtr eptr = Marshal.GetFunctionPointerForDelegate(profileEventDelegate);
+            ahkThread.LoadFile("ProfileThread.ahk");
+            ahkThread.Eval(String.Format("ph := new ProfileHandler({0})", eptr));
+        }
+
+        // Used for eg reloading user settings on load of app
+        public bool SetHotkey(string hkName, uint keyCode)
+        {
+            ahkThread.Eval(String.Format("ph.SetHotkey(\"{0}\", {1})", hkName, keyCode));
+            return true;
+        }
+
+        // User requested change of hotkey
+        public bool SelectHotkey()
+        {
+
+            return true;
+        }
+
+        private void profileEventCallback(string hkName, bool evt, uint keyCode)
+        {
+            //Console.WriteLine("Profile: " + evt + ", " + keyCode);
+            profileCallback(hkName, evt, keyCode);
+        }
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        delegate void ProfileEvent(
+            [MarshalAs(UnmanagedType.LPWStr)]string hkName,
+            [MarshalAs(UnmanagedType.Bool)]bool evt,
+            [MarshalAs(UnmanagedType.U4)]uint keyCode);
+
+    }
 }
--- src/HotkeyWrapperTest/Program.cs
+++ src/HotkeyWrapperTest/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace HotkeyWrapperTest
@@ -11,6 +12,19 @@ namespace HotkeyWrapperTest
         static void Main(string[] args)
         {
             var hw = new HotkeyWrapper();
+            //hw.AddProfile("Default");
+            hw.SetHotkey("hk1", 65, new Action<bool>((value) => 
+            {
+                Console.WriteLine("Hotkey 1 Value: " + value);
+            }));
+
+            hw.SetHotkey("hk2", 66, new Action<bool>((value) => 
+            {
+                Console.WriteLine("Hotkey 2 Value: " + value);
+            }));
+
+            while (true)
+                Thread.Sleep(100);
         }
     }
 }
